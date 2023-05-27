f2b2d9b45716d4397a90097388bac3563020e827
Matias G <dikimatig@gmail.com>
2023-05-27 17:48:28 -0300

    componente footer agregado sigo maquetando

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-
+import { Container } from 'react-bootstrap'
+import Footer from './componets/footer'
 function App() {
   
 
   return (
     <>
-      
+    <Container className='main'>
+    <h1 className='text-center py-4'>Lista De Tareas!</h1>
+    <hr />
+    </Container>
+    <Footer></Footer>
     </>
   )
 }
--- src/componets/footer.jsx
+++ src/componets/footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const footer = () => {
+const Footer = () => {
     return (
-        <footer>
-            
+        <footer className='bg-dark text-center text-light py-3'>
+            <p>&copy; Todos Los Derechos Reservados</p>
         </footer>
     );
 };
 
-export default footer;
+export default Footer;
